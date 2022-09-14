1f1b202a60c4a4908c8ba9cbeb45f07cf731e449
simon <soesia18@htl-kaindorf.at>
2022-09-14 17:44:43 +0200

    Init

--- src/main/java/at/kaindorf/console/Main.java
+++ src/main/java/at/kaindorf/console/Main.java
@@ -6,6 +6,7 @@ import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -21,6 +22,19 @@ public class Main {
 
 
         List<Schoolclass> list;
+
+
+//        try {
+//            list = Files.lines( Paths.get(System.getProperty("user.dir"), "src", "main", "resources", "schooldata.csv"), StandardCharsets.UTF_8)
+//                    .skip(1)
+//                    .map(line -> {
+//                        String[] lineData = line.split(";");
+//                        return new Schoolclass(lineData[0], lineData[1], lineData[2], lineData[3], lineData[4], Integer.parseInt(lineData[5]), lineData[6]);
+//                    })
+//                    .collect(Collectors.toList());
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
 
         try (BufferedReader br = new BufferedReader(new FileReader(file))) {
             list = br.lines().skip(1).map(line -> {
