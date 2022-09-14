0cb2dcfd0afd7c88362a9a9173fb184b69cbc564
simon <soesia18@htl-kaindorf.at>
2022-09-14 17:40:13 +0200

    Init

---
+++ src/main/java/at/kaindorf/console/Main.java
@@ -0,0 +1,48 @@
+package at.kaindorf.console;
+
+import at.kaindorf.pojo.Schoolclass;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        File file =
+                Paths
+                        .get(System.getProperty("user.dir"), "src", "main", "resources", "schooldata.csv")
+                        .toFile();
+
+
+        List<Schoolclass> list;
+
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            list = br.lines().skip(1).map(line -> {
+                String[] lineData = line.split(";");
+                return new Schoolclass(lineData[0], lineData[1], lineData[2], lineData[3], lineData[4], Integer.parseInt(lineData[5]), lineData[6]);
+            }).collect(Collectors.toList());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        list.forEach(System.out::println);
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("PU_testdb");
+        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+
+        list.forEach(em::persist);
+
+        em.getTransaction().commit();
+
+        em.close();
+        emf.close();
+    }
+}
